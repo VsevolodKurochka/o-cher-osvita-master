ca7e26026f3f63c2f30457f75c29e6332ffbf0b3
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-01-16 01:20:52 +0200

    Fixed Navigation scrolling

--- app/js/vanilla.main.js
+++ app/js/vanilla.main.js
@@ -76,7 +76,7 @@ var exists = function exists(element) {
 		// Navigation links
 
 		var scroll = new SmoothScroll('a[href*="#"]', {
-			speed: 1000
+			offset: 70
 		});
 
 		var jsNavLinks = document.querySelectorAll('.nav__menu a[href*="#"]');
@@ -84,15 +84,8 @@ var exists = function exists(element) {
 		for (var i = 0; i < jsNavLinks.length; i++) {
 			jsNavLinks[i].addEventListener('click', function (e) {
 
-				e.preventDefault();
-
-				var vnavhref = this.getAttribute("href").replace("#", "");
-				var vnavscrollAnchor = document.getElementById(vnavhref);
-
 				removeClass(jsNavBtn, classes.active);
 				removeClass(jsNav, classes.menuActive);
-
-				//scrollTo(document.body, vnavscrollAnchor.offsetTop, 600);
 			});
 		}
 
